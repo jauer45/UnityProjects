b3af18475b7d5c5ead5f616e15c1f5683d851a12
Joseph Auer <joeauer@gmx.com>
2017-10-20 11:15:35 +0100

    C# 32-bit Unity initial test development. Everything should be here to be opened in Unity 5.6.3+ as setup from the Microsoft Visual Studio Community Edition, (Open Source distro off Microsoft's github page), and built.

---
+++ Readme.md
@@ -0,0 +1,6 @@
+C# Unity Development Area.
+
+
+Currently just feeling my way through the Unity dev environment. But I have a game idea:
+
+   Ballr - A portamento on Steve Ballmer and Rollerball. The Game is just in the phase of initial learning demo standard (using the Unity Demos). But there is a full development-road layout. The alpha demo will be based upon an old 80s arcade game similar to Crystal Castles crossed with qbert crossed withi billiards.  
---
+++ Test/Assets/scripts/PlayerController.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    public Text countText;
+    public Text winText;
+
+    private Rigidbody rb;
+    private int count;
+
+    public AudioClip pickupSound;
+    private AudioSource source;
+    private float volLowRandse = 0.5f;
+    private float volHighRandse = 1.0f;
+
+    //Set onLoad
+    private void Awake()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    // Init PlayerController 
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        setCountText();
+        winText.text = "";
+    }
+
+    // Physics Logic and Event Stuff set/called here
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            float vol = Random.Range(volLowRandse, volHighRandse);
+            other.gameObject.SetActive(false);
+            source.PlayOneShot(pickupSound, vol);
+            count = count + 1;
+            setCountText();
+        }
+    }
+
+    void setCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if( count >= 9 )
+        {
+            winText.text = "You Win !!!";
+        }
+    }
+
+
+}
